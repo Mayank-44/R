92c0385fa2fc4b1dbffc75333b03a7e6adad42f6
Mayank Gangwar <37243315+Mayank-44@users.noreply.github.com>
2018-04-21 23:11:04 +0530

    Add files via upload
    sorting vector

---
+++ vector.R
@@ -0,0 +1,13 @@
+v<-c(1,4,3,5,2)
+sort_result<-sort(v)
+print(sort_result)
+
+sort_result<-sort(v,decreasing=TRUE)
+print(sort_result)
+
+color <-c("red","resulr","resulo","resulp")
+sort_result<-sort(color)
+print(sort_result)
+
+sort_result<-sort(color,decreasing=TRUE)
+print(sort_result)
