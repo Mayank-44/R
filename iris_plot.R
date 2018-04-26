144d39d8bde30d6710814631460f9f337505f6d9
Mayank Gangwar <37243315+Mayank-44@users.noreply.github.com>
2018-04-26 22:38:08 +0530

    Add files via upload
    plot the graphs of iris dataset

---
+++ iris_plot.R
@@ -0,0 +1,8 @@
+attach(iris)
+iris
+
+plot(density(iris$Sepal.Length),main = "Density of sepal length")
+
+plot(density(iris$Sepal.Width), main = "Density of sepal width")
+
+plot(hist(iris$Sepal.Length),main = "histogram of sepal length")
