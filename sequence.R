d5e65d853804cad5b3cf9e2b4a8e419ba6da4f7f
Mayank Gangwar <37243315+Mayank-44@users.noreply.github.com>
2018-04-27 23:41:06 +0530

    create sequence

---
+++ sequence.R
@@ -0,0 +1,12 @@
+# creating a sequence 
+s <- 1:10
+print(s)
+
+# creating a sequence of even number
+s <- seq(0,10, by=2)
+print (s)
+
+# creating a sequence of odd number
+s <- seq(1,10, by=2)
+print (s)
+
